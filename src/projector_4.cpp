f9785efb6c7da55cadf43f68e23428e79f37c667
MickeyKen <mickey333ff@gmail.com>
2019-02-16 21:44:45 +0900

    print out homography matrix usinsg std::

--- src/projector_4.cpp
+++ src/projector_4.cpp
@@ -26,15 +26,23 @@ void Callback(const std_msgs::Int16& msg)
     std::string file_dir = ros::package::getPath("experiment_miki") + "/src/image/";
     std::string input_file_path = file_dir + "pop_90.png";
     cv::Mat source_img = cv::imread(input_file_path, cv::IMREAD_UNCHANGED);
+    const cv::Point2f src_pt[]={
+             cv::Point2f(0.0, 0.0),
+             cv::Point2f(1023.0 , 0.0),
+             cv::Point2f(1023.0 , 767.0),
+             cv::Point2f(0.0, 767.0)};
+
+    const cv::Point2f dst_pt[]={
+             cv::Point2f(-620.0, 2660.0),
+             cv::Point2f(620.0 , 2660.0),
+             cv::Point2f(390.0 , 1320.0),
+             cv::Point2f(-390.0, 1320.0)};
+    const cv::Mat homography_matrix = cv::getPerspectiveTransform(src_pt,dst_pt);
+    std::cout << "M = "<< std::endl << " "  << homography_matrix << std::endl << std::endl;
 
     ros::Rate rate(30);
     while (ros::ok()) {
-      const cv::Point2f src_pt[]={
-               cv::Point2f(0.0, 0.0),
-               cv::Point2f(1023.0 , 0.0),
-               cv::Point2f(1023.0 , 767.0),
-               cv::Point2f(0.0, 767.0)};
-      printf("x: %f y: %f", src_pt[2].x, src_pt[2].y);
+      //printf("x: %f y: %f", src_pt[2].x, src_pt[2].y);
       cv::imshow("screen_4", source_img);
       cv::waitKey(1);
       n.getParam("exp_miki_img/switch", fin_switch);
