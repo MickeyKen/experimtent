21f11400f8c8557b8afec07a55ee0d626384a106
MickeyKen <mickey333ff@gmail.com>
2019-02-28 22:11:27 +0900

    [Add] out_and_back.py

--- src/control_experiment.py
+++ src/control_experiment.py
@@ -5,9 +5,88 @@ import tf
 from std_msgs.msg import Float64
 import time
 from ubiquitous_display_pantilt.msg import Pantilt
+from geometry_msgs.msg import Twist, Point, Quaternion
+import PyKDL
+from math import radians, copysign, sqrt, pow, pi
 
-def move(x, y):
-    print x, y
+def quat_to_angle(quat):
+    rot = PyKDL.Rotation.Quaternion(quat.x, quat.y, quat.z, quat.w)
+    return rot.GetRPY()[2]
+
+def get_odom():
+    tf_listener = tf.TransformListener()
+    try:
+        tf_listener.waitForTransform("odom", "base_footprint", rospy.Time(), rospy.Duration(1.0))
+    except (tf.Exception, tf.ConnectivityException, tf.LookupException):
+        rospy.loginfo("Cannot find transform between /odom and /ud_base_footprint")
+        rospy.signal_shutdown("tf Exception")
+    try:
+        (trans, rot) = tf_listener.lookupTransform("odom", "base_footprint", rospy.Time(0))
+    except (tf.Exception, tf.ConnectivityException, tf.LookupException):
+        rospy.loginfo("TF Exception")
+        return
+    return (Point(*trans), Quaternion(*rot))
+
+def move(curren_pos, target_pos, x, y):
+    # print x, y
+    pub_vel = rospy.Publisher('cmd_vel',Twist, queue_size=10)
+    vel_msg = Twist()
+    position = Point()
+    rotation = Quaternion()
+
+    current_x_distance = 0.0
+    current_y_distance = 0.0
+
+
+    # pub_vel.publish(vel_msg)
+
+    ### get initial position
+    (position, rotation) = get_odom()
+    initial_x = position.x
+    initial_y = position.y
+
+    if curren_pos < target_pos:
+        if (x == 0.0):
+            pass
+        else:
+            vel_msg.linear.x = 0.4
+            pub_vel.publish(vel_msg)
+            while( current_x_distance < x):
+                (position, rotation) = get_odom()
+                current_x_distance = sqrt(pow((position.x - initial_x), 2))
+            vel_msg.linear.x = 0.0
+            pub_vel.publish(vel_msg)
+        if (y == 0.0):
+            pass
+        else:
+            vel_msg.linear.y = 0.3
+            pub_vel.publish(vel_msg)
+            while( current_y_distance < y):
+                (position, rotation) = get_odom()
+                current_y_distance = sqrt(pow((position.y - initial_y), 2))
+            vel_msg.linear.y = 0.0
+            pub_vel.publish(vel_msg)
+    else:
+        if (y == 0.0):
+            pass
+        else:
+            vel_msg.linear.y = 0.3
+            pub_vel.publish(vel_msg)
+            while( current_y_distance < y):
+                (position, rotation) = get_odom()
+                current_y_distance = sqrt(pow((position.y - initial_y), 2))
+            vel_msg.linear.y = 0.0
+            pub_vel.publish(vel_msg)
+        if (x == 0.0):
+            pass
+        else:
+            vel_msg.linear.x = 0.4
+            pub_vel.publish(vel_msg)
+            while( current_x_distance < x):
+                (position, rotation) = get_odom()
+                current_x_distance = sqrt(pow((position.x - initial_x), 2))
+            vel_msg.linear.x = 0.0
+            pub_vel.publish(vel_msg)
 
 
 def exp_control():
@@ -41,7 +120,7 @@ def exp_control():
 
         pub_pan.publish(float_pan)
         pub_tilt.publish(float_tilt)
-        time.sleep(10)
+        time.sleep(1)
 
         if a == 1 or a == 4 or a == 7:
             rospy.set_param("/exp_num",a)
@@ -49,10 +128,10 @@ def exp_control():
             if (current_pos == 1):
                 pass
             elif (current_pos == 2):
-                move(-2.5, -1.0)
+                move(current_pos, 1, -2.5, -1.0)
 
             elif (current_pos == 3):
-                move(-2.5, -3.7)
+                move(current_pos, 1, -2.5, -3.7)
 
             else:
                 pass
@@ -70,13 +149,13 @@ def exp_control():
             rospy.set_param("/exp_num",a)
 
             if (current_pos == 1):
-                move(2.5, 1.0)
+                move(current_pos, 2, 2.5, 1.0)
 
             elif (current_pos == 2):
                 pass
 
             elif (current_pos == 3):
-                move(0, -2.7)
+                move(current_pos, 2, 0.0, -2.7)
 
             else:
                 pass
@@ -94,10 +173,10 @@ def exp_control():
             rospy.set_param("/exp_num",a)
 
             if (current_pos == 1):
-                move(2.5, 3.7)
+                move(current_pos, 3, 2.5, 3.7)
 
             elif (current_pos == 2):
-                move(0.0, 2.7)
+                move(current_pos, 3, 0.0, 2.7)
 
             elif (current_pos == 3):
                 pass
