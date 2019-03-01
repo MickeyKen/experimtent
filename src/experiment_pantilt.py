5fe46c2428078971ba62a648bf83c8f02b5a4d0e
Kenta Miki <mickey333ff@gmail.com>
2019-03-01 19:07:12 +0900

    [Add] get param exp_num

--- src/experiment_pantilt.py
+++ src/experiment_pantilt.py
@@ -1,22 +1,34 @@
+#!/usr/bin/env python
+
 import rospy
-from std_msgs.msg import String
+from std_msgs.msg import Float64,Int16
+import time
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+    print data.data
 
-def listener():
+    time.sleep(1)
+
+    pub_tilt = rospy.Publisher('tilt_controller/command', Float64, queue_size=10)
+
+    exp_num = rospy.get_param("/exp_num")
+
+
+
+
+
+
+def experiment_pantilt():
 
     # In ROS, nodes are uniquely named. If two nodes with the same
     # name are launched, the previous one is kicked off. The
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+    rospy.init_node('experiment_for_pantilt', anonymous=True)
 
-    rospy.Subscriber('chatter', String, callback)
-
-    # spin() simply keeps python from exiting until this node is stopped
+    rospy.Subscriber("finish_pantilt", Int16, callback)
     rospy.spin()
 
 if __name__ == '__main__':
-    listener()
+    experiment_pantilt()
